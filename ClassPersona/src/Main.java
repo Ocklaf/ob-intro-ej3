3f671c06e1b4a4326f1ff47f311d2e3ab1efd48e
Ocklaf <ocklaf@gmail.com>
2023-07-18 11:10:20 +0200

    Finished

--- ClassPersona/src/Main.java
+++ ClassPersona/src/Main.java
@@ -1,17 +1,14 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Persona newPerson = new Persona();
 
-        // Press Mayús+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        newPerson.setEdad(39);
+        newPerson.setNombre("José Vicente Falcó");
+        newPerson.setTelefono("+34685959113");
 
-            // Press Mayús+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        System.out.println(newPerson.getNombre());
+        System.out.println(newPerson.getEdad());
+        System.out.println(newPerson.getTelefono());
     }
 }
